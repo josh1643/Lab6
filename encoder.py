1866e0af2fc887474908fe757cb6ee48f795a4c8
josh1643 <joshua.wasserman@ufl.edu>
2023-10-25 16:25:14 -0400

    main function

--- encoder.py
+++ encoder.py
@@ -12,3 +12,32 @@ def decode(password):
         decoded_pass += str((int(digit) - 3))
     return decoded_pass
 
+def print_menu():
+    print('Menu')
+    print('-------------')
+    print('1. Encode')
+    print('2. Decode')
+    print('3. Quit')
+    print()
+    print('Please enter an option:', end = " ")
+
+
+def main():
+    go = True
+    while go:
+        print_menu()
+        option = input()
+        if option == '1':
+            print('Please enter your password to encode:', end=" ")
+            password = encode(input())
+            print('Your password has been encoded and stored!')
+            print()
+        if option == '2':
+            print(f'The encoded password is {password} and the original password is ')
+            print()
+        if option == '3':
+            go = False
+
+
+if __name__== '__main__':
+    main()
